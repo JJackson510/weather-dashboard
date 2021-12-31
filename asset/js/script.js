ae14a968a510961a0491c8fa1072a7ee60ea228d
JJackson510 <jamiej.jj5@gmail.com>
2021-12-31 13:02:01 -0600

    started js

---
+++ asset/js/script.js
@@ -0,0 +1,18 @@
+var cityEl = document.getElementById("enter-city");
+var searchEl = document.getElementById("search-button");
+var clearEl = document.getElementById("clear-history");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindEl = document.getElementById("wind-speed");
+var currentUVEl = document.getElementById("UV-index");
+var historyEl = document.getElementById("history");
+var fiveDayEl = document.getElementById("fiveday-header");
+var todayWeatherEl = document.getElementById("today-weather");
+var apiKey = "0cb8906a01cde2e8e9d6527a0953b593";
+
+
+
+
+
